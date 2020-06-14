abc857513c803d7961be91698966779d457e4638
Rob Gauer <gauerr@gmail.com>
2020-06-14 01:05:19 -0500

    Mission to Mars Web Scraping Challenge - Rob Gauer

--- Mission_to_Mars/Code_Versions/app.py
+++ Mission_to_Mars/Code_Versions/app.py
@@ -1,43 +1,37 @@
 from flask import Flask, render_template, request
-#from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-import os
+import sys
 
-# Create an instance of Flask
 app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
-# Use PyMongo to establish Mongo connection
-#mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_data")
-
-app.config['MONGO_URI'] = "mongodb://localhost:27017/mars_data"
-mongo = PyMongo(app)
+#app.config['MONGO_URI'] = "mongodb://localhost:27017/mars_data"
+#mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
 
     # Find one record of data from the mongo database
+    #mars_data = mongo.db.mars_data.find_one()
     mars_data = mongo.db.mars_data.find_one()
-    
+
     # Return template and data
-    return render_template("index.html", mars_data=mars_data)
+    return render_template("index.html", mars_db=mars_data)
+
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape()
+    mars_db = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
     #mongo.db.mars_data.update({}, mars_data, upsert=True)
-    #return 'All done!'
-    mongo.db.mars_data.update({}, mars_data, upsert=True)
-
-    # Redirect back to home page
-    #time.sleep(20)
-    return redirect("/")
+    mongo.db.mars_data.update({}, mars_db, upsert=True)
+    return 'Mars Data Update Complete!'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Mission_to_Mars/Code_Versions/app_version2.py
+++ Mission_to_Mars/Code_Versions/app_version2.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 import scrape_mars
-
+import sys
 
 app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
-app.config['MONGO_URI'] = "mongodb://localhost:27017/mars_data"
-mongo = PyMongo(app)
+#app.config['MONGO_URI'] = "mongodb://localhost:27017/mars_data"
+#mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
 
     # Find one record of data from the mongo database
+    #mars_data = mongo.db.mars_data.find_one()
     mars_data = mongo.db.mars_data.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_data=mars_data)
+    return render_template("index.html", mars_db=mars_data)
 
 
 # Route that will trigger the scrape function
@@ -24,11 +26,12 @@ def index():
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape()
+    mars_db = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.mars_data.update({}, mars_data, upsert=True)
-    return 'All done!'
+    #mongo.db.mars_data.update({}, mars_data, upsert=True)
+    mongo.db.mars_data.update({}, mars_db, upsert=True)
+    return 'Mars Data Update Complete!'
 
 if __name__ == "__main__":
     app.run(debug=True)
